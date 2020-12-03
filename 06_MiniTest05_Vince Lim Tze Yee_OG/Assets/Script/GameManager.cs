b0a2ada888cea2ec7b299d4065c0033ff8c4a5e8
YukimiSnow <73086228+YukimiSnow@users.noreply.github.com>
2020-12-03 19:44:33 +0800

    Yes
    Yes

---
+++ 06_MiniTest05_Vince Lim Tze Yee_OG/Assets/Script/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public float levelTime;
+
+    public GameObject TimeText;
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public int numberOfSpawn;
+    public int TotalCubeSize;
+    public int AddCubeSize;
+    public int MinusCubeSize;
+    public int AddCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        //This is used to spawn cubes on random spots
+        for (int i = 0; i < numberOfSpawn; i++)
+        {
+            //Spawn location for X Y Z
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15),
+                                            0,
+                                            Random.Range(-15, 15));
+            //Randomly spawning between add or minus energy
+            if(Random.Range(0,2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is to count the total number of Cubes respectively
+        AddCubeSize = GameObject.FindGameObjectsWithTag("AddEnergy").Length; //Counting Add Cubes
+        MinusCubeSize = GameObject.FindGameObjectsWithTag("MinusEnergy").Length; //Counting Minus Cube
+
+        TotalCubeSize = AddCubeSize + MinusCubeSize; //Counting Total Cube
+
+        if (levelTime > 0)
+        {
+            levelTime -= Time.deltaTime;
+            //print("levelTime : " + levelTime);
+            //print("levelTime : " + FormatTime(levelTime));
+            TimeText.GetComponent<Text>().text = "Time Left : " + levelTime;
+        }
+        else
+        {
+            levelTime = 0;
+            //print("Times Up!");
+            TimeText.GetComponent<Text>().text = "Times up!";
+        }
+
+    }
+    //Method to add time and spawn cubes
+    public void AddTime()
+    {
+
+        for (int i = 0; i < AddCube; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15),
+                                            0,
+                                            Random.Range(-15, 15));
+            if (Random.Range(0, 2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+
+        levelTime += 5;
+    }
+    //Formatting the time to count 00(Mins):00(Seconds):00(MiliSec) 
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 *(time - minutes * 60 - seconds));
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+    }
+}
+
---
+++ 06_MiniTest05_Vince Lim Tze Yee_OG/Assets/Script/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10.0f;
+    bool TimeRunning;
+
+    public GameObject AddCountText;
+    public GameObject MinusCountText;
+    public GameObject TotalEnergyText;
+    public GameObject EnergyCountText;
+    public Animator PlayerAni;
+    public int energyCount;
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.GetComponent<Rigidbody>(); //Caling Rigidbody for collision checks for private
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement Forward
+        if (Input.GetKey(KeyCode.W) && TimeRunning == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            PlayerAni.SetBool("RunningBool", true);
+
+        }
+        //Movement Backwards
+        if (Input.GetKey(KeyCode.S) && TimeRunning == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            PlayerAni.SetBool("RunningBool", true);
+        }
+        //Movement Left
+        if (Input.GetKey(KeyCode.A) && TimeRunning == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            PlayerAni.SetBool("RunningBool", true);
+        }
+        //Movement Right
+        if (Input.GetKey(KeyCode.D) && TimeRunning == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            PlayerAni.SetBool("RunningBool", true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.W)|| Input.GetKeyUp(KeyCode.A)|| Input.GetKeyUp(KeyCode.S)|| Input.GetKeyUp(KeyCode.D) && TimeRunning == true)
+        {
+            PlayerAni.SetBool("RunningBool", false);
+        }
+
+
+        //Printing text onto the screen to display the following.
+        EnergyCountText.GetComponent<Text>().text = "Energy Collected : " + energyCount; //Showing Energy Collected
+
+        AddCountText.GetComponent<Text>().text = "Add Energy Left : " + GameManager.instance.AddCubeSize; //Total Add Cube 
+
+        MinusCountText.GetComponent<Text>().text = "Minus Energy Left : " + GameManager.instance.MinusCubeSize;//Total Minus Cube
+
+        TotalEnergyText.GetComponent<Text>().text = "Total Energy Left : " + GameManager.instance.TotalCubeSize;// Total Cube
+
+        if (energyCount <= -1 || GameManager.instance.levelTime <= 0)
+        {
+            TimeRunning = false;
+
+            SceneManager.LoadScene("LoseScene");
+        }
+        else if (energyCount >= 50)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+        else
+        {
+            TimeRunning = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision Player)
+    {
+        //if Player colide with addcube it adds time, energy collected and spawn more cubes.
+        if (Player.gameObject.CompareTag("AddEnergy"))
+        {
+            energyCount += 5;
+
+            GameManager.instance.AddTime();
+
+            Destroy(Player.gameObject);
+        }
+        if (Player.gameObject.CompareTag("MinusEnergy"))
+        {
+            energyCount -= 25;
+            Destroy(Player.gameObject);
+        }
+
+    }
+}
